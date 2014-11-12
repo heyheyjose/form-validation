27bda72a824ce8bc2968567c616b48e4460440b8
heyheyjose <heyheyjose@users.noreply.github.com>
2014-11-12 13:52:51 -0500

    added bg images and randomized them..so cool!

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
+var images = ['1.jpg', '2.jpg', '3.jpg', '4.jpg', '5.jpg', '6.jpg', '7.jpg', '8.jpg'];
+
+$('body').css({'background-image': 'url(images/' + images[Math.floor(Math.random() * images.length)] + ')'});
+
 $('form').validate({
 	rules: {
 		name: {
